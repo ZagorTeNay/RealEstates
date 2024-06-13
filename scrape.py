71bdd4d7aee10798ffb8c7ba080b72b581f0d729
Radivoje <rneskovicsbb@gmail.com>
2024-06-13 17:43:39 +0200

    Crawled information being saved to dataframe and then appended to csv. First small .csv file with data created

--- scrape.py
+++ scrape.py
@@ -1,8 +1,8 @@
-import csv
 import re
 import urllib.request
 from bs4 import BeautifulSoup
 from time import sleep
+import pandas as pd
 
 NEKRETNINE_URL = 'https://www.nekretnine.rs'
 NEKRETNINE_FIRST_PART_OF_URL = 'https://www.nekretnine.rs/stambeni-objekti/'
@@ -16,14 +16,14 @@ def get_data(max_pages, type_of_estate):
     while page <= max_pages:
         source = urllib.request.urlopen(url).read()
         soup = BeautifulSoup(source, 'html.parser')
+
         for link in soup.find_all('div', class_='col-8 col-md-9 offer-body py-2 px-3'):
             href = link.find('a').get('href')
             crawl_advert(NEKRETNINE_URL + href)
-            break
         page += 1
 
         next_page = soup.find('link', rel='next')
-        sleep(1)
+        sleep(0.5)
 
 
 def crawl_advert(href):
@@ -34,9 +34,11 @@ def crawl_advert(href):
     print('Here')
     print(href)
 
-    location = soup.find('h3', class_='stickyBox__Location').get_text().strip()
+    crawled_information = {}
 
-    res = {}
+    location = soup.find('h3', class_='stickyBox__Location').get_text().strip()
+    crawled_information['location'] = location
+
     # Every second one is not good, so we skip it. TO DO: Check if the text could be retrieved better
     cnt = 1
     for tag in soup.find('div', class_='property__main-details').find_all('span'):
@@ -52,7 +54,7 @@ def crawl_advert(href):
         property_value = re.sub('[^A-Za-z0-9 ]+', '', splitted_text[1])
         property_value = ' '.join(property_value.split())
 
-        res[property_name] = property_value
+        crawled_information[property_name] = property_value
 
         cnt += 1
 
@@ -66,14 +68,8 @@ def crawl_advert(href):
         property_value = re.sub('[^A-Za-z0-9 ]+', '', splitted_text[1])
         property_value = ' '.join(property_value.split())
 
-        res[property_name] = property_value
+        crawled_information[property_name] = property_value
 
-
-    sptxt = soup.find('div', class_='property__main-details').getText().strip()
-    re.sub(r'\n\s*\n', '\n\n', sptxt)
-
-    crawled_information['location'] = location
-    print(location)
     insert_crawled_data_into_json(crawled_information)
 
     return
@@ -81,11 +77,10 @@ def crawl_advert(href):
 
 def insert_crawled_data_into_json(crawled_information):
     print(crawled_information)
-    with open("sample.csv", "w", encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerow(crawled_information)
+    df = pd.DataFrame([crawled_information])
+    df.to_csv('out.csv', mode='a', encoding='utf-8')
 
 
 list_of_estate_types = ['stanovi', 'kuce']
 for estate_type in list_of_estate_types:
-    get_data(1, estate_type)
+    get_data(3, estate_type)
